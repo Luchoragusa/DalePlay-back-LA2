d1bf3c90dd420f20126a0f73e6d99cd12a06cf9e
Luchoragusa <luchito.rc4@gmail.com>
2022-08-27 21:47:30 -0300

    add findGamesByDeveloper

--- src/controllers/models/game.controller.js
+++ src/controllers/models/game.controller.js
@@ -13,6 +13,20 @@ const findGamesByCategory = async (req,res) => {
     }
 };
 
+const findGamesByDeveloper = async (req,res) => {
+    const id = req.params.id
+    let games = await Game.findAll({ where: { idDeveloper: id } });
+    // Si obtengo un array vacio, es porque no hay juegos de esa categoria
+    // Puse el .length porque sino me daba error
+    if (games.length > 0) { 
+        return res.status(200).json({games, 'msg':'Encontrados correctamente'})
+    } else {
+        // Si no hay juegos de esa categoria, devuelvo un 404
+        return res.status(404).json({'msg':'No hay datos'})
+    }
+};
+
 module.exports = {
-    findGamesByCategory
+    findGamesByCategory,
+    findGamesByDeveloper
 };
