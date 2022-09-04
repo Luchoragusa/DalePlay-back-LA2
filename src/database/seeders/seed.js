7d38205ce22fbc010f2eae1a09b8661e64bd9fbb
Luchoragusa <luchito.rc4@gmail.com>
2022-09-03 22:20:02 -0300

    update image dev

--- src/database/seeders/seed.js
+++ src/database/seeders/seed.js
@@ -12,10 +12,12 @@ const categories = [
 ];
 
 const developers = [
-    { name: 'Codemasters', image: 'https://cdn.discordapp.com/attachments/852889034723426324/1013239177300684820/codemasters-grid-image.jpg.adapt.crop191x100.628p.jpg'} ,
-    { name: 'EA', image: 'https://cdn.discordapp.com/attachments/852889034723426324/1013239176117895208/1200px-EA_Sports_monochrome_logo.svg.png'} ,
-    { name: 'Rockstar', image: 'https://cdn.discordapp.com/attachments/852889034723426324/1013239177581694986/Rockstar_Games_Logo.svg.png'} ,
-    { name: 'Riot Games', image: 'https://cdn.discordapp.com/attachments/852889034723426324/1013239176692498482/riot-games.webp'} ,
+    { name: 'Codemasters', image: 'https://frroutneduar-my.sharepoint.com/:i:/g/personal/lragusa_frro_utn_edu_ar/ERKe2_y24yBBjfwC74LO4-ABYSqmscGPGmzkR9Ng4u50BQ?e=YjPBH1'} ,
+    { name: 'EA', image: 'https://frroutneduar-my.sharepoint.com/:i:/g/personal/lragusa_frro_utn_edu_ar/EXrTe0vcL1xJhiicade8528BCqMnsveiN0xaJvSCn6JW4Q?e=xI8HcC'} ,
+    { name: 'Rockstar', image: 'https://frroutneduar-my.sharepoint.com/:i:/g/personal/lragusa_frro_utn_edu_ar/ETWiSiTvzPxKrZ5QEJfj5eoBkqZWP3iFIQd10vEyDU8NUg?e=8KaJs6'} ,
+    { name: 'Riot Games', image: 'https://frroutneduar-my.sharepoint.com/:i:/g/personal/lragusa_frro_utn_edu_ar/EVZPFcFrMiNGhwIVNL4YSacBKDIeEqecODTfyCrb-AiCTg?e=ytPGOi'} ,
+    { name: 'Playground Games', image: 'https://frroutneduar-my.sharepoint.com/:i:/g/personal/lragusa_frro_utn_edu_ar/ESKWR6N3TmxCmWJPOPVs0LABq0nOFKP4XJ_ip-Kx0wLJcg?e=KwhckJ'} ,
+    { name: 'Mediatronic Games', image: 'https://frroutneduar-my.sharepoint.com/:i:/g/personal/lragusa_frro_utn_edu_ar/EUz9buXwqEhCqF2mbkUCwpABdDcH0KgmPBQii5ktRA-9NA?e=PotS1y'}
 ];
 
 const users = [
@@ -64,6 +66,12 @@ const games = [
         valoration: 5,
         description: 'F1 2022 es un videojuego de carreras de automóviles desarrollado por Codemasters Birmingham y publicado por Codemasters. Es la vigésimo segunda entrega de la serie F1 y se lanzó el 16 de julio de 2022 para PlayStation 5, PlayStation 4, Xbox Series X, Xbox Series S, Xbox One, Nintendo Switch, Google Stadia y Microsoft Windows.',
     },
+    {
+        name: 'F1 21',
+        idCategory: 3,
+        idDeveloper: 2,
+        
+    }
 ];
 
 const usergames  = [
