97d0c22847e7d2f34bfd261a2059aefe36ef4263
Luchoragusa <luchito.rc4@gmail.com>
2022-09-06 14:24:16 -0300

    add placeholder id seed is null

--- src/helpers/externalApi.js
+++ src/helpers/externalApi.js
@@ -1,13 +1,18 @@
 const axios = require('axios');
 
 let url = 'https://randomuser.me/api/?seed=';
+let placeholder = 'https://cdn.discordapp.com/attachments/852889034723426324/1016760215267446865/placeholder.png';
 
 const externalApi = async (seed) => {
     try{
-        let urlFinal = url + seed;
-        let response = await axios.get(urlFinal);
-        let urlImage = await response.data.results[0].picture.large;
-        return urlImage;
+        if (seed !== null) {
+            let urlFinal = url + seed;
+            let response = await axios.get(urlFinal);
+            let urlImage = await response.data.results[0].picture.large;
+            return urlImage;
+        } else {
+            return placeholder;
+        }
     } catch (error) {
         console.log(error);
     }
