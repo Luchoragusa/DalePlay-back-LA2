c6ee17949c2427095eeec281dbc9eabd19d03e5e
Luchoragusa <luchito.rc4@gmail.com>
2022-09-09 00:58:20 -0300

    fix jeje

--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -416,7 +416,7 @@ const sendPurchasenEmail = async (user, game) => {
                         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                           <tr>
                             <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
-                              <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">¡Felicitacion por adquirir <span style="color: #1e90ff;">${game.name}</span>!</h1>
+                              <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">¡Felicitaciones por adquirir <span style="color: #1e90ff;">${game.name}</span>!</h1>
                             </td>
                           </tr>
                         </table>
