d5809fe1c7665bf89ac743dff56a909eaa960401
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2022-09-11 19:00:05 -0300

    fix contact controller

--- src/controllers/models/contact.controller.js
+++ src/controllers/models/contact.controller.js
@@ -1,22 +1,25 @@
-const { Contact, User } = require('../../database/models/index');
+const { Contact, User } = require("../../database/models/index");
 
-const create =  async (req, res) => {
-    try{
-        const id = req.userId;
-        const user = await User.findByPk(id);
-        const contact = await Contact.create({email: user.email, message: req.body.message});
-        if (contact) {
-            return res.status(200).json({'msg':'Creado correctamente', contact})
-        } else {
-            return res.status(404).json({'msg':'No se recibieron los datos'})
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ msg: 'Error en el servidor' });
+const create = async (req, res) => {
+  try {
+    const id = req.userId;
+    const user = await User.findByPk(id);
+    const contact = await Contact.create({
+      email: user.email,
+      reason: req.body.reason,
+      message: req.body.message,
+    });
+    if (contact) {
+      return res.status(200).json({ msg: "Creado correctamente", contact });
+    } else {
+      return res.status(404).json({ msg: "No se recibieron los datos" });
     }
-}
-
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error en el servidor" });
+  }
+};
 
 module.exports = {
-    create
+  create,
 };
